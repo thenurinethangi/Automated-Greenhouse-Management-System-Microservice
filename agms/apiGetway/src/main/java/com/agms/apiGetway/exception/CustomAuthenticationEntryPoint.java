a10513ce5607b8fcc7830dcd720f408c167b10b8
thenurinethangi <thenurinathangi@gmail.com>
2026-04-03 23:10:17 +0530

    complete service registry

--- agms/apiGetway/src/main/java/com/agms/apiGetway/exception/CustomAuthenticationEntryPoint.java
+++ agms/apiGetway/src/main/java/com/agms/apiGetway/exception/CustomAuthenticationEntryPoint.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import com.agms.apiGetway.utils.APIResponse;
-import com.agms.apiGetway.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
